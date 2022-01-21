3111e76a7fd937822cc863059bf0668a052ca875
Florens Grimm <fgrimm@ep1.rub.de>
2022-01-21 23:42:02 +0100

    Adds profile loading

--- gui/loadui.py
+++ gui/loadui.py
@@ -151,7 +151,7 @@ class UI(QtWidgets.QMainWindow):
 
         # set profile names connections
         self.profile_name_line_edit.returnPressed.connect(self.save_profile)
-        files = sorted(self.paths['profiles'].glob('*.json'), key=lambda name: name.upper())
+        files = sorted(self.paths['profiles'].glob('*.json'), key=lambda name: name.stem.upper())
         for file in files:
             print(file)
             self.profile_name_combo_box.addItem(file.stem, file)
