d0d5c38db243ea08077501f1a425871cec426a75
Florens Grimm <grimm@aept.rub.de>
2022-01-21 15:09:25 +0100

    Cleanup

--- camera.py
+++ camera.py
@@ -10,23 +10,44 @@ class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         
-        self.setLayout(QtWidgets.QVBoxLayout())
+        self.cam = PiCamera()
 
-        self.take_pic_button = QtWidgets.QPushButton("Take a picture", self)
-        self.take_pic_button.clicked.connect(self.take_pic)
-        self.layout().addWidget(self.take_pic_button)
+        self.setLayout(QtWidgets.QVBoxLayout())
+        
+        self.start_preview_button = QtWidgets.QPushButton("Start Preview", self)
+        self.start_preview_button.clicked.connect(self.start_preview)
+        self.layout().addWidget(self.start_preview_button)
+
+        self.stop_preview_button = QtWidgets.QPushButton("Stop Preview", self)
+        self.stop_preview_button.clicked.connect(self.stop_preview)
+        self.layout().addWidget(self.stop_preview_button)
+        
+        self.pic_button = QtWidgets.QPushButton("Take a picture", self)
+        self.pic_button.clicked.connect(self.take_pic)
+        self.layout().addWidget(self.pic_button)
+
+        self.brightness_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.brightness_slider.valueChanged[int].connect(self.set_brightness)
+        self.layout().addWidget(self.brightness_slider)
 
         self.show()
     
-    def take_pic(self):
-        cam = PiCamera()
-        cam.resolution = (1920, 1080)
-        cam.brightness = 70
-        cam.start_preview()
+    def start_preview(self):
+        self.cam.resolution = (1920, 1080)
+        
+        self.cam.start_preview(fullscreen=False, window=(1000, 500, 800, 640))
+        
         sleep(2)
-        cam.capture('foo.jpg')
+    
+    def stop_preview(self):
+        self.cam.stop_preview()
 
-        cam.close()
+    def take_pic(self):
+        self.cam.capture('foo.jpg')
+
+    def set_brightness(self, value):
+        self.cam.brightness = value
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
