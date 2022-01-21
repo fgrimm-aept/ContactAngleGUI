0ee4f46bb08431f80d4b6ea920e6c56df20d2edb
Florens Grimm <fgrimm@ep1.rub.de>
2022-01-21 16:36:25 +0100

    Resizes Frame Window

--- gui/loadui.py
+++ gui/loadui.py
@@ -94,7 +94,7 @@ class UI(QtWidgets.QMainWindow):
         self.cam.iso = value
 
     def start_preview(self):
-        self.cam.start_preview(fullscreen=False, window=(830, 70, 1080, 764))
+        self.cam.start_preview(fullscreen=False, window=(830, 70, 1280, 764))
 
     def stop_preview(self):
         self.cam.stop_preview()
