b4ddbcf4f841451e5e2576cdc349a49f01cb38ff
Florens Grimm <fgrimm@ep1.rub.de>
2022-01-23 05:11:24 +0100

    Implements Keyboard shortcuts

--- gui/loadui.py
+++ gui/loadui.py
@@ -10,6 +10,7 @@ from picamera import PiCamera
 
 # TODO: QSettings benutzen um root -> XOFFSET,YOFFSET und user -> XOFFSET,YOFFSET zu speichern?
 # TODO: Fix setting directory for cam picture
+# TODO: Set Tooltips for all elements (at least for quality)
 
 class WorkerThread(QtCore.QThread):
     TIMESTAMP = QtCore.pyqtSignal(str)
