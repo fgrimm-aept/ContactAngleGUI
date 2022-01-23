dabab4109c13075f198b8644ace524f0cf47fa23
Florens Grimm <fgrimm@ep1.rub.de>
2022-01-23 17:56:15 +0100

    Implements Directory setting

--- gui/loadui.py
+++ gui/loadui.py
@@ -188,7 +188,7 @@ class UI(QtWidgets.QMainWindow):
         self.preview_shortcut_space.activated.connect(self.toggle_preview)
         self.take_pic_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_T), self)
         self.take_pic_shortcut.activated.connect(self.take_pic)
-        self.take_pic_shortcut_space = QtWidgets.QShortcut(QtGui.QKeySequence(f'Ctrl + {QtCore.Qt.Key_Space}'),
+        self.take_pic_shortcut_space = QtWidgets.QShortcut(QtGui.QKeySequence(f'Shift + {QtCore.Qt.Key_Space}'),
                                                            self)
         self.take_pic_shortcut_space.activated.connect(self.take_pic)
 
